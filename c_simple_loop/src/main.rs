ee1161c03e5266157279a2a3d82f5472c8281b45
ayders22 <ayders22@pm.me>
2026-06-12 21:43:05 +0200

    Add c_simple_loop

---
+++ c_simple_loop/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut counter = 0;
+
+    loop {
+        println!("counter value: {:?}", counter);
+        counter += 1;
+        if counter == 10 {
+            break;
+        }
+    }
+}
